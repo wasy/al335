666846575e98ba0bacd5bf795ccc3602b9c7590f
wasy <wasy@tut.by>
2011-10-01 22:46:31 +0300

    some fix DuelReset

---
+++ src/server/scripts/Custom/duel_reset.cpp
@@ -0,0 +1,45 @@
+#include "ScriptPCH.h"
+
+enum Zones
+{
+    ZONE_ELWYNN_FOREST  = 12,
+    ZONE_DUROTAR        = 14,
+    ZONE_DALARAN        = 4395,
+};
+
+class Reset_OnDuelEnd : public PlayerScript
+{
+    public:
+        Reset_OnDuelEnd() : PlayerScript("Reset_OnDuelEnd") {}
+
+    void OnDuelEnd(Player* winner, Player* loser, DuelCompleteType type)
+    {
+        if (type == DUEL_WON)
+        {
+            switch (winner->GetZoneId())
+            {
+                case ZONE_ELWYNN_FOREST:
+                case ZONE_DUROTAR:
+                case ZONE_DALARAN:
+                    winner->RemoveArenaSpellCooldowns();
+                    loser->RemoveArenaSpellCooldowns();
+
+                    winner->SetHealth(winner->GetMaxHealth());
+                    loser->SetHealth(loser->GetMaxHealth());
+
+                    if (winner->getPowerType() == POWER_MANA)
+                        winner->SetPower(POWER_MANA, winner->GetMaxPower(POWER_MANA));
+
+                    if (loser->getPowerType() == POWER_MANA)
+                        loser->SetPower(POWER_MANA, loser->GetMaxPower(POWER_MANA));
+
+                    break;
+            }
+        }
+    }
+};
+
+void AddSC_DuelReset()
+{
+    new Reset_OnDuelEnd;
+}
